b72ec770c6bc37e3e78631d8c0821ee2debe781c
hayato <aa@gmail.com>
2017-05-08 16:55:33 +0900

    no message

---
+++ 20170508/test2.php
@@ -0,0 +1,9 @@
+<?php
+$a = "hello world";
+echo $a;
+echo $"<br>";
+$b = 8;
+echo $b;
+$c = 100 + 5;
+echo $c;
+?>
---
+++ 20170508/test3.php
@@ -0,0 +1,14 @@
+
+<?php
+
+   define("CONSTANT1","Hello PHP");
+
+   print(CONSTANT1);
+
+   print("<br/>");
+
+   const CONSTANT2 = "Hello PHP 5.3" ;
+
+    print(CONSTANT2);
+
+?>
